7d15b8446114174e6b4e188f6317276ff6cdc6d2
aben20807 <aben20807@gmail.com>
2018-02-14 11:00:50 +0800

    Fixed size bug

--- algorithm/heap_sort/heap_sort.cpp
+++ algorithm/heap_sort/heap_sort.cpp
@@ -28,11 +28,11 @@ void max_heapify(vector<int> *A, int i)
     int l = get_left(i);
     int r = get_right(i);
     int largest;
-    if (l <= (*A).size() && (*A)[l] > (*A)[i])
+    if (l <= (*A).size() - 1 && (*A)[l] > (*A)[i])
         largest = l;
     else
         largest = i;
-    if (r <= (*A).size() && (*A)[r] > (*A)[largest])
+    if (r <= (*A).size() - 1 && (*A)[r] > (*A)[largest])
         largest = r;
     if (largest != i) {
         swap((*A)[i], (*A)[largest]);
@@ -43,11 +43,11 @@ void max_heapify(vector<int> *A, int i)
 void heap_sort(vector<int> *A)
 {
     /* Build max heap */
-    for (int i = (*A).size() / 2; i >= 1; i--) {
+    for (int i = (*A).size() - 1 / 2; i >= 1; i--) {
         max_heapify(&(*A), i);
     }
-    vector<int> ans;
-    for (int i = (*A).size(); i >= 2; i--) {
+    vector<int> ans(0);
+    for (int i = (*A).size() - 1; i >= 2; i--) {
         int t = (*A)[1];
         swap((*A)[1], (*A)[i]);
         (*A).pop_back();
@@ -60,7 +60,7 @@ void heap_sort(vector<int> *A)
 
 int main()
 {
-    vector<int> a({0, 16, 4, 10, 14, 7, 9, 3, 2, 8, 1});
+    vector<int> a({-1, 16, 4, 10, 14, 7, 9, 3, 2, 8, 1});
     print_vector(a);
     heap_sort(&a);
     print_vector(a);
