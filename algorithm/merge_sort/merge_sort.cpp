115dcda9fd72fbe8be8ff4c1c883f6f69496f35c
aben20807 <aben20807@gmail.com>
2018-02-14 12:35:43 +0800

    Format merge sort

--- algorithm/merge_sort/merge_sort.cpp
+++ algorithm/merge_sort/merge_sort.cpp
@@ -28,30 +28,27 @@ void merge(vector<int> *A, int p, int q, int r)
     R[n2 + 1] = INT_MAX;
     int i = 1;
     int j = 1;
-    for (int k = p; k <= r; k++){
-        if (L[i] <= R[j]){
-            (*A)[k] = L[i];
-            i++;
-        } else {
-            (*A)[k] = R[j];
-            j++;
-        }
+    for (int k = p; k <= r; k++) {
+        if (L[i] <= R[j])
+            (*A)[k] = L[i++];
+        else
+            (*A)[k] = R[j++];
     }
 }
 
 void merge_sort(vector<int> *A, int p, int r)
 {
-    if (p < r){
-        int q = (p + r)/ 2;
+    if (p < r) {
+        int q = (p + r) / 2;
         merge_sort(A, p, q);
-        merge_sort(A, q+1, r);
+        merge_sort(A, q + 1, r);
         merge(A, p, q, r);
     }
 }
 
 int main()
 {
-    vector<int> a({0, 16, 4, 10, 14, 7, 9, 3, 2, 8, 1});
+    vector<int> a({-1, 16, 4, 10, 14, 7, 9, 3, 2, 8, 1});
     print_vector(a);
     merge_sort(&a, 1, a.size() - 1);
     print_vector(a);
