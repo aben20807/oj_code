22176e118a1a00c28093168210da65b9240fda32
aben20807 <aben20807@gmail.com>
2018-03-19 16:54:51 +0800

    539 clean code

--- UVa/539_The_Settlers_of_Catan.cpp
+++ UVa/539_The_Settlers_of_Catan.cpp
@@ -15,17 +15,8 @@ void dfs(int i, int d)
         if (edge[i][x]) {
             if (maxstep < d)
                 maxstep = d;
-            // printf("%d->%d: %d\n", i, x, d);
             edge[i][x] = 0;
             edge[x][i] = 0;
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    // printf("%d", edge[i][j]);
-                }
-                // printf("\n");
-            }
-            // printf("----------------\n");
-            // d++;
             dis[x] = dis[i] + 1;
             dfs(x, d + 1);
             edge[i][x] = 1;
@@ -46,25 +37,14 @@ int main()
             edge[i][j] = 1;
             edge[j][i] = 1;
         }
-        // for (int i = 0; i < n; i++){
-        // for (int j = 0; j < n; j++){
-        // printf("%d", edge[i][j]);
-        // }
-        // printf("\n");
-        // }
-        // memset(dis, 0, sizeof(dis));
         int maxmaxstep = 0;
         for (int i = 0; i < n; i++) {
             memset(dis, 0, sizeof(dis));
             maxstep = 0;
             int d = 1;
             dfs(i, d);
-            for (int j = 0; j < n; j++) {
-                // printf("%d ", dis[j]);
-            }
             if (maxmaxstep < maxstep)
                 maxmaxstep = maxstep;
-            // printf("\n");
         }
         printf("%d\n", maxmaxstep);
     }
