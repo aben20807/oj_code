22dbf192b90f360cf5a0fcbe0fc8cd2b01998759
aben20807 <aben20807@gmail.com>
2018-03-20 21:12:53 +0800

    11987 maybe work mess code

--- UVa/11987_Almost_Union-Find.cpp
+++ UVa/11987_Almost_Union-Find.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <set>
+#include <map>
+#include <vector>
 using namespace std;
+
+vector<vector<int>> myset;
+map<int, int> root;
 
 int myfind(int x, int p[])
 {
@@ -10,11 +16,26 @@ int myfind(int x, int p[])
     return p[x] = myfind(p[x], p);
 }
 
-void myunion(int x, int y, int p[])
+void myunion(int x, int y)
 {
-    int X = myfind(x, p);
-    int Y = myfind(y, p);
-    p[X] = Y;
+    // int X = myfind(x, p);
+    // int Y = myfind(y, p);
+    // p[X] = Y;
+    int X = root[x];
+    int Y = root[y];
+    if (myset[X].size() > myset[Y].size()) {
+        for (int n : myset[Y]) {
+            myset[X].push_back(n);
+            root[n] = X;
+        }
+        myset[Y].clear();
+    } else {
+        for (int n : myset[X]) {
+            myset[Y].push_back(n);
+            root[n] = Y;
+        }
+        myset[X].clear();
+    }
 }
 
 void printset(int pa[], int n)
@@ -29,9 +50,15 @@ int main()
 {
     int n, m;
     while (scanf("%d%d", &n, &m) != EOF) {
+        myset.clear();
+        root.clear();
         int pa[n + 1] = {0};
         for (int i = 0; i <= n; i++) {
-            pa[i] = i;
+            // pa[i] = i;
+            root[i] = i;
+            vector<int> tmp;
+            tmp.push_back(i);
+            myset.push_back(tmp);
         }
         while (m--) {
             int command, p, q;
@@ -40,45 +67,62 @@ int main()
             if (command == 1) {
                 scanf("%d%d", &p, &q);
                 // printf("%d %d:\n", p, q);
-                if (myfind(p, pa) != myfind(q, pa)) {
-                    myunion(p, q, pa);
+                // if (myfind(p, pa) != myfind(q, pa)) {
+                if (root[p] != root[q]) {
+                    // myunion(p, q, pa);
+                    myunion(p, q);
                     // printset(pa, n);
                 }
             } else if (command == 2) {
                 scanf("%d%d", &p, &q);
                 // printf("%d %d:\n", p, q);
-                if (myfind(p, pa) != myfind(q, pa)) {
-                    if (myfind(p, pa) == pa[p]) {
-                        // if move element is root, find a new root for others
-                        int newroot;
-                        for (int i = 0; i <= n; i++) {
-                            if (myfind(i, pa) == myfind(p, pa) && i != p) {
-                                newroot = i;
-                                break;
-                            }
-                        }
-                        for (int i = 0; i <= n; i++) {
-                            if (myfind(i, pa) == myfind(p, pa) && i != p) {
-                                pa[i] = newroot;
-                            }
-                        }
-                    }
-                    pa[p] = myfind(q, pa);
+                // if (myfind(p, pa) != myfind(q, pa)) {
+                if (root[p] != root[q]) {
+                    // if (myfind(p, pa) == pa[p]) {
+                    // // if move element is root, find a new root for others
+                    // int newroot;
+                    // for (int i = 0; i <= n; i++) {
+                    // if (myfind(i, pa) == myfind(p, pa) && i != p) {
+                    // pa[i] = i;
+                    // newroot = i;
+                    // break;
+                    // }
+                    // }
+                    // // myunion(p, newroot, pa);
+                    // myunion(p, newroot);
+                    // // for (int i = 0; i <= n; i++) {
+                    // // if (myfind(i, pa) == myfind(p, pa) && i != p) {
+                    // // pa[i] = newroot;
+                    // // }
+                    // // }
+                    // }
+                    // pa[p] = myfind(q, pa);
                     // printset(pa, n);
+                    auto iter = myset[root[p]].begin();
+                    while (*iter != p) ++iter;
+                    myset[root[p]].erase(iter);
+                    myset[root[q]].push_back(p);
+                    root[p] = root[q];
                 }
             } else {
                 scanf("%d", &p);
                 // printf("%d:\n", p);
                 int pset = myfind(p, pa);
-                int cnt = 0, sum = 0;
                 // printset(pa, n);
                 for (int i = 0; i <= n; i++) {
                     if (myfind(i, pa) == pset) {
-                        cnt++;
-                        sum += i;
+                        // cnt++;
+                        // sum += i;
                     }
                 }
+                int cnt = 0, sum = 0;
+                for (int i = 0; i < (int)myset[root[p]].size(); i++) {
+                    cnt++;
+                    sum += myset[root[p]][i];
+                    // printf("%d", myset[root[p]][i]);
+                }
                 printf("%d %d\n", cnt, sum);
+                // printf("\n");
             }
         }
     }
