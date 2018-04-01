8526ba70c0276b4edea11cd00c698dabc68bc66a
aben20807 <aben20807@gmail.com>
2018-04-01 21:42:53 +0800

    p4 still wa

--- google_code_jam/p4_Bathroom_Stalls.cpp
+++ google_code_jam/p4_Bathroom_Stalls.cpp
@@ -8,39 +8,24 @@ using namespace std;
 int main()
 {
     int t, casecnt = 1;
-    scanf("%d", &t);
+    scanf("%d\n", &t);
     while (t--) {
         i64 n, k;
-        scanf("%lld%lld", &n, &k);
+        scanf("%lld %lld\n", &n, &k);
         printf("Case #%d: ", casecnt++);
         queue<i64> q;
         q.push(n);
         while (k--) {
             i64 ttop = q.front();
             q.pop();
-            if (ttop % 2 == 0) {
-                i64 r = ttop / 2;
-                i64 l = ttop / 2 - 1;
-                if (k == 0) {
-                    printf("%lld %lld\n", r, l);
-                    break;
-                }
-                if (r > 0)
-                    q.push(r);
-                if (l > 0)
-                    q.push(l);
-            } else {
-                i64 r = ((ttop - 1) / 2);
-                i64 l = r;
-                if (k == 0) {
-                    printf("%lld %lld\n", r, l);
-                    break;
-                }
-                if (r > 0)
-                    q.push(r);
-                if (l > 0)
-                    q.push(l);
+            i64 r = ttop / 2;
+            i64 l = (ttop - 1) / 2;
+            if (k == 0) {
+                printf("%lld %lld\n", r, l);
+                break;
             }
+            q.push(r);
+            q.push(l);
         }
     }
     return 0;
