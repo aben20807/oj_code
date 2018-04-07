73c53e2bb2596ac2dcdf03c54b5f04285b124b95
aben20807 <aben20807@gmail.com>
2018-04-07 21:01:33 +0800

    p4 add support when area > sqrt(2)

--- google_code_jam/Qualification_Round_2018/p4_Cubic_UFO.cpp
+++ google_code_jam/Qualification_Round_2018/p4_Cubic_UFO.cpp
@@ -33,6 +33,26 @@ int main()
                     r = i;
                 }
             }
+        } else {
+            double l = 0.0, r = 45.0;
+            while (l < r) {
+                double i = (l + r) / 2;
+                double rad = i * PI / 180.0;
+                double tmpa = (2 * sin(rad) + sqrt(2) * cos(rad));
+                if (abs(tmpa - a) < 0.000000000000001) {
+                    printf("%.16lf %.16f %.16f\n", -1 * cos(rad) / 2, sin(rad) / 2, 0.0);
+                    double x = sqrt(2) * sin(rad) / 2;
+                    double y = sqrt(2) * cos(rad) / 2;
+                    printf("%.16lf %.16f %.16f\n", x / 2, y / 2, sqrt(2) / 4);
+                    printf("%.16lf %.16f %.16f\n", x / 2, y / 2, -1 * sqrt(2) / 4);
+                    break;
+                }
+                if (tmpa < a) {
+                    l = i;
+                } else {
+                    r = i;
+                }
+            }
         }
     }
     return 0;
