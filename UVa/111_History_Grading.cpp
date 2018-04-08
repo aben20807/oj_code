732cb34b4dfd3f969a4538f168eb40d88f14663a
aben20807 <aben20807@gmail.com>
2018-04-08 14:07:39 +0800

    111 maybe finish

--- UVa/111_History_Grading.cpp
+++ UVa/111_History_Grading.cpp
@@ -6,25 +6,51 @@
 #include <vector>
 using namespace std;
 
-// int lcs()
-// {
-// }
+int lcs(vector<int> c, vector<int> r)
+{
+    int dp[c.size() + 1][r.size() + 1];
+    memset(dp, 0, sizeof(dp));
+    for (int i = 0; i < (int)c.size(); i++) {
+        for (int j = 0; j < (int)r.size(); j++) {
+            // printf("%d ", dp[i][j]);
+        }
+        // printf("\n");
+    }
+    for (int i = 1; i < (int)c.size(); i++) {
+        for (int j = 1; j < (int)r.size(); j++) {
+            if (c[i] == r[j]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    for (int i = 0; i <= (int)c.size(); i++) {
+        for (int j = 0; j <= (int)r.size(); j++) {
+            // printf("%d ", dp[i][j]);
+        }
+        // printf("\n");
+    }
+    return dp[c.size() - 1][r.size() - 1];
+}
 
 int main()
 {
     string tmps;
-    int n;
+    int n = 0;
     vector<int> c;
     while (getline(cin, tmps)) {
         if (tmps.length() > 0 && (int)tmps.find(" ") == -1) {
             // memset(c, 0, sizeof(c));
-            c.clear();
+            // c.clear();
             n = atoi(tmps.c_str());
+            c.resize(n + 1);
+            fill(c.begin(), c.end(), 0);
             // printf("%d\n", n);
-            for (int i = 0; i < n; i++) {
+            for (int i = 1; i <= n; i++) {
                 int tmpn;
                 scanf("%d", &tmpn);
-                c.push_back(tmpn);
+                c[tmpn] = i;
             }
             for (int i = 0; i < n; i++) {
                 // printf("%d ", c[i]);
@@ -32,21 +58,23 @@ int main()
             // printf("\n");
         } else if (tmps.length() > 0) {
             stringstream ss(tmps);
-            vector<int> r;
-            r.clear();
-            int tmpn;
+            vector<int> r(n + 1);
+            fill(r.begin(), r.end(), 0);
+            int tmpn, index = 1;
             while (ss >> tmpn) {
                 // printf("%d ", tmpn);
-                r.push_back(tmpn);
+                // r.push_back(tmpn);
+                r[tmpn] = index++;
             }
             for (int i : c) {
-                printf("%d ", i);
+                // printf("%d ", i);
             }
-            printf("\n");
+            // printf("\n");
             for (int i : r) {
-                printf("%d ", i);
+                // printf("%d ", i);
             }
-            printf("\n");
+            // printf("\n");
+            printf("%d\n", lcs(c, r));
         }
     }
     return 0;
